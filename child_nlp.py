37e245e7a7240453a8156dd64a808c91a7c96016
bergsonz <zachary.bergson@dowjones.com>
2019-07-02 13:56:09 -0400

    first pass phrase matcher

---
+++ child_nlp.py
@@ -0,0 +1,21 @@
+import spacy
+from spacy.matcher import PhraseMatcher
+nlp = spacy.load("en_core_web_sm")
+matcher = PhraseMatcher(nlp.vocab)
+
+terms = []
+with open('./mental-terms.txt', 'r') as wordDoc:
+    for line in wordDoc:
+      currentPlace = line[:-1]
+      terms.append(currentPlace)
+
+
+# Only run nlp.make_doc to speed things up
+patterns = [nlp.make_doc(text) for text in terms]
+matcher.add("TerminologyList", None, *patterns)
+print('tony')
+doc = nlp(u"To avoid upsetting his aunt. Might be short tempered or have a bad meltdown when angry.")
+matches = matcher(doc)
+for match_id, start, end in matches:
+    span = doc[start:end]
+    print(span.text)
