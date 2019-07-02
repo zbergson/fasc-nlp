89a93d0564853e21d1fd52f25141c3a1d8731f88
bergsonz <zachary.bergson@dowjones.com>
2019-07-02 13:59:42 -0400

    clean

--- child_nlp.py
+++ child_nlp.py
@@ -13,7 +13,6 @@ with open('./mental-terms.txt', 'r') as wordDoc:
 # Only run nlp.make_doc to speed things up
 patterns = [nlp.make_doc(text) for text in terms]
 matcher.add("TerminologyList", None, *patterns)
-print('tony')
 doc = nlp(u"To avoid upsetting his aunt. Might be short tempered or have a bad meltdown when angry.")
 matches = matcher(doc)
 for match_id, start, end in matches:
